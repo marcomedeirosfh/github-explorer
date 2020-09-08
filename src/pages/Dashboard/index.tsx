50bfca0b3c944df10d684c9fdaf06d4eb4d8b4b3
Marco Medeiros <marcomedeirosfilho@gmail.com>
2020-09-08 22:05:46 +0100

    Added styled components and  finished dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,60 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore Github Repositories</Title>
+
+      <Form>
+        <input placeholder="Insert repository name" />
+        <button type="submit">Search</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars2.githubusercontent.com/u/33945119?s=460&v=4"
+            alt="Marco Medeiros"
+          />
+          <div>
+            <strong>marcomedeirosfh/github-explorer</strong>
+            <p>Explore github repositories with React JS</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://avatars2.githubusercontent.com/u/33945119?s=460&v=4"
+            alt="Marco Medeiros"
+          />
+          <div>
+            <strong>marcomedeirosfh/github-explorer</strong>
+            <p>Explore github repositories with React JS</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://avatars2.githubusercontent.com/u/33945119?s=460&v=4"
+            alt="Marco Medeiros"
+          />
+          <div>
+            <strong>marcomedeirosfh/github-explorer</strong>
+            <p>Explore github repositories with React JS</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
